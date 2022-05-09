99cc016369255bece2c7194afc8910d2d838b8ea
Munsa1 <munsamibenge65@gmail.com>
2022-05-09 14:35:37 +0200

    Add Scooter description

--- src/app/components/MyFavourites.js
+++ src/app/components/MyFavourites.js
@@ -6,7 +6,9 @@ class MyFavourites extends Component {
      return (
        <div>
          <section className="card-container">
-          <img src="https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA="/>
+          <img src="https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA=" className="card-img"/>
+          <h2>Sccoter Description</h2>
+          
          </section>
        </div>
      )
