744edb3f20f1b6e3eb8943619f2f07c3d4e850da
Munsa1 <munsamibenge65@gmail.com>
2022-05-09 14:40:36 +0200

    Indent code properly for readability purposes

--- src/app/components/MyFavourites.js
+++ src/app/components/MyFavourites.js
@@ -6,11 +6,11 @@ class MyFavourites extends Component {
      return (
        <div>
          <section className="card-container">
-          <img src="https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA=" className="card-img"/>
-          <div className="card-description">
-            <h2>Sccoter Description</h2>
-            <p>$ 3600</p>
-          </div>
+            <img src="https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA=" className="card-img"/>
+              <div className="card-description">
+                <h2>Sccoter Description</h2>
+                <p>$ 3600</p>
+            </div>
          </section>
        </div>
      )
