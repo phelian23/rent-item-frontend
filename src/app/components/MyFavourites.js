d597ca28d575e0d6e5b21e37f1939f36f4b432b3
Munsa1 <munsamibenge65@gmail.com>
2022-05-09 18:36:32 +0200

    Add placeholder images and use bootstrap cards

--- src/app/components/MyFavourites.js
+++ src/app/components/MyFavourites.js
@@ -5,13 +5,44 @@ class MyFavourites extends Component {
    render() {
      return (
        <div>
-         <section className="card-container">
-            <img src="https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA=" className="card-img"/>
-              <div className="card-description">
-                <h2>Sccoter Description</h2>
-                <p>$ 3600</p>
-            </div>
-         </section>
+         <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA=" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://media.istockphoto.com/photos/blue-scooter-with-white-background-picture-id1041315464?k=20&m=1041315464&s=612x612&w=0&h=ro2tosXYLDrBsKqKTioXdUKDah8DLdu2ggxK-ZZRRe8=" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJP4z5K8uIcVekb6uM3uxtBDSjQcUc-sOfk8UFwons8gEJKnqz-L2ADfUnBhR2GXuoLZc&usqp=CAU" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuQQt992w6pmjU_BnL_Kt9SWHbdBlW4QBP6n5lacebvL0pkpspd2Hn13l-8_YcWx_zz-0&usqp=CAU" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
        </div>
      )
    }
