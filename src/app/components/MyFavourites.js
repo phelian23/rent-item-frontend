1de4c15909e581dd2a0ada217eb2c3f0b3198cd4
Munsa1 <munsamibenge65@gmail.com>
2022-05-09 18:49:41 +0200

    Add media query and resize card images

--- src/app/components/MyFavourites.js
+++ src/app/components/MyFavourites.js
@@ -5,7 +5,7 @@ class MyFavourites extends Component {
    render() {
      return (
        <div>
-         <div class="row row-cols-1 row-cols-md-2 g-4">
+         <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card">
       <img src="https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA=" class="card-img-top" alt="..."/>
@@ -36,6 +36,24 @@ class MyFavourites extends Component {
   <div class="col">
     <div class="card">
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuQQt992w6pmjU_BnL_Kt9SWHbdBlW4QBP6n5lacebvL0pkpspd2Hn13l-8_YcWx_zz-0&usqp=CAU" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://techcrunch.com/wp-content/uploads/2021/12/bounce-infinity-e1.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUskD0dRRrP144DXEKF1Tr8dB4oEQjQkfdQ&usqp=CAU" class="card-img-top" alt="..."/>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
