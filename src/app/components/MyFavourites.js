40f72b6830e4ec8bd883b49b29e606db8647de9a
Munsa1 <munsamibenge65@gmail.com>
2022-05-10 10:47:46 +0200

    Add URL's to the array

--- src/app/components/MyFavourites.js
+++ src/app/components/MyFavourites.js
@@ -9,34 +9,39 @@ const MyFavourites  = ()=>{
     {
       id: 1,
       title: "Classic Blue",
-      price: "$ 3,600"
+      price: "$ 3,600",
+      image: "https://media.istockphoto.com/photos/classic-vintage-scooter-motor-bike-or-moped-isolated-on-whte-picture-id1348520235?b=1&k=20&m=1348520235&s=170667a&w=0&h=VMQrftBuUWaSwO70pti7DjLnDc4AG8Y9QWIGqzvguZA="
     },
     {
       id: 2,
       title: "Light Blue",
-      price: "$ 2,500"
+      price: "$ 2,500",
+      image: "https://media.istockphoto.com/photos/blue-scooter-with-white-background-picture-id1041315464?k=20&m=1041315464&s=612x612&w=0&h=ro2tosXYLDrBsKqKTioXdUKDah8DLdu2ggxK-ZZRRe8="
     },
     {
       id: 3,
       title: "White Red",
-      price: "$ 4,000"
+      price: "$ 4,000",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJP4z5K8uIcVekb6uM3uxtBDSjQcUc-sOfk8UFwons8gEJKnqz-L2ADfUnBhR2GXuoLZc&usqp=CAU"
     },
     {
       id: 4,
       title: "Black Orange",
-      price: "$ 5,400"
+      price: "$ 5,400",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuQQt992w6pmjU_BnL_Kt9SWHbdBlW4QBP6n5lacebvL0pkpspd2Hn13l-8_YcWx_zz-0&usqp=CAU"
     },
     {
       id: 5,
       title: "Red Black",
-      price: "$ 3,500"
+      price: "$ 3,500",
+      image: "https://techcrunch.com/wp-content/uploads/2021/12/bounce-infinity-e1.jpg"
     },
     {
       id: 6,
       title: "Maroon Black",
-      price: "$ 4,200"
+      price: "$ 4,200",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUskD0dRRrP144DXEKF1Tr8dB4oEQjQkfdQ&usqp=CAU"
     }
-
   ]
 }
 export default MyFavourites
