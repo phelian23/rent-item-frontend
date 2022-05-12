c12907ee50a261af6a0d732692f2ae7af66be43a
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-05-12 12:02:28 +0400

    Update favourites page

--- src/app/components/MyFavourites.js
+++ src/app/components/MyFavourites.js
@@ -1,35 +1,42 @@
-import React, {useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import { getAllFavourites } from '../redux/actions/favourites'
-import './myfavourites.css'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllFavourites } from '../redux/actions/favourites';
+import './myfavourites.css';
 
+const MyFavourites = () => {
+  const dispatch = useDispatch();
+  const favourites = useSelector((state) => state.favourites.favourites);
+  const scooters = JSON.parse(localStorage.getItem('scooters'));
 
-
-const MyFavourites = ()=> {
-  const dispatch = useDispatch()
-  const favourites = useSelector((state) => state.favourites.favourites)
+  /* eslint-disable arrow-body-style */
+  const favouriteScooters = scooters.filter((scooter) => {
+    return favourites.find((favourite) => favourite.item_id === scooter.id);
+  });
 
   useEffect(() => {
-    dispatch(getAllFavourites())
-  }, [dispatch])
-  
-  return(
+    dispatch(getAllFavourites());
+  }, [dispatch]);
+
+  return (
     <div>
-<div class="row row-cols-1 row-cols-md-3 g-4">
-{favourites.map(favourite => ( 
-  <div class="col">
-    <div class="card" key={favourite.id}>
-      <img src={favourite.image} class="card-img-top" alt="..." />
-      <div class="card-body d-flex justify-content-between">
-        <h5 class="card-title">{favourite.title}</h5>
-        <p class="card-text">{favourite.price}</p>
+      <div className="text-center p-3">
+        <h1>My Favourites</h1>
+      </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {favouriteScooters.map((favourite) => (
+          <div className="col" key={favourite.id}>
+            <div className="card">
+              <img src={favourite.photo} className="card-img-top" alt="..." />
+              <div className="card-body d-flex justify-content-between">
+                <h5 className="card-title">{favourite.name}</h5>
+                <p className="card-text">{favourite.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  </div>
- ))}
- </div>
-    </div>
-  )
-}
+  );
+};
 
-export default MyFavourites
+export default MyFavourites;
